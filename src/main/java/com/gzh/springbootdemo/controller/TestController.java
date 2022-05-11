93782ec6fa8d77d6985abad2b96f4d8798ce136d
yuyufeifei <964148400@qq.com>
2022-05-11 10:28:24 +0800

    第一次提交

---
+++ src/main/java/com/gzh/springbootdemo/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.gzh.springbootdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "test success";
+    }
+}
