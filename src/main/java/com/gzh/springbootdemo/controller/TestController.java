14435ab0b08431fef484965c3ddd668ac2c31ce8
yuyufeifei <964148400@qq.com>
2022-05-11 11:00:08 +0800

    测试

--- src/main/java/com/gzh/springbootdemo/controller/TestController.java
+++ src/main/java/com/gzh/springbootdemo/controller/TestController.java
@@ -1,9 +1,9 @@
 package com.gzh.springbootdemo.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TestController {
     @GetMapping("/test")
     public String test() {
